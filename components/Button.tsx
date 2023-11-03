f7fcd77fe656c57870aa81dacd3ae43e37944dd2
Picvajzmen <picvajzmen@gmail.com>
2023-11-03 13:48:25 +0100

    Added ServiceSection and ServiceBox comps

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,14 +2,14 @@ import { ChildrenProps } from "@/types/ChildrenProps";
 import React from "react";
 
 const Button = (props: ChildrenProps) => {
-	return (
-		<button className="flex gap-4 rounded-3xl bg-primaryAccent px-3 py-2">
-			{props.children}
-			<svg className="w-[24px] h-[24px]">
-				<use xlinkHref={`./images/sprite.svg#button-arrow`}></use>
-			</svg>
-		</button>
-	);
+  return (
+    <button className="flex gap-4 rounded-3xl bg-primaryAccent px-3 py-2 group">
+      {props.children}
+      <svg className="w-[24px] h-[24px] group-hover:translate-x-1 transition-transform">
+        <use xlinkHref={`./images/sprite.svg#button-arrow`}></use>
+      </svg>
+    </button>
+  );
 };
 
 export default Button;
---
+++ components/ServiceBox.tsx
@@ -0,0 +1,37 @@
+import { ChildrenProps } from "@/types/ChildrenProps";
+import Image from "next/image";
+import React, { ReactNode } from "react";
+
+type TServiceBoxProps = {
+  icon: string;
+  subservices?: string[];
+  service?: string;
+};
+
+const ServiceBox = (props: TServiceBoxProps) => {
+  return (
+    <article className="flex gap-5 rounded-3xl bg-secondaryAccent p-6 ">
+      <svg
+        className="lg:w-[80px] lg:h-[80px] sm:w-[70px] sm:h-[70px] w-[40px]
+h-[40px] shrink-0"
+      >
+        <use xlinkHref={`./images/sprite.svg#${props.icon}`}></use>
+      </svg>
+      <div>
+        <h4 className="font-bold text-xl">{props.service}</h4>
+        <ul className="flex flex-wrap gap-2 mt-2">
+          {props.subservices?.map((subservice, index) => (
+            <li key={index} className="flex flex-nowrap basis-[44%]">
+              <svg className="w-[24px] h-[24px] shrink-0">
+                <use xlinkHref={`./images/sprite.svg#dot-line`}></use>
+              </svg>
+              {subservice}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </article>
+  );
+};
+
+export default ServiceBox;
