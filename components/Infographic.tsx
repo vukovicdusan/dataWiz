78cd953240224ddae4c67d7aa75dc28eea60acac
vukovicdusan <dusan.v.vukovic@gmail.com>
2023-11-09 22:25:02 +0100

    Moved infographic css from global.css to Infographic.module.css

--- components/Infographic.tsx
+++ components/Infographic.tsx
@@ -1,52 +1,64 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
+import styles from "./Infographic.module.css";
 
 const Infographic = () => {
-  let animationRef = useRef<HTMLOListElement>(null);
-  const [show, setShow] = useState(false);
+	let animationRef = useRef<HTMLOListElement>(null);
+	const [show, setShow] = useState(false);
 
-  useEffect(() => {
-    let config = {
-      threshold: 0.5,
-    };
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setShow(true);
-        }
-      });
-    }, config);
+	useEffect(() => {
+		let config = {
+			threshold: 0.5,
+		};
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					setShow(true);
+				}
+			});
+		}, config);
 
-    if (animationRef.current) {
-      observer.observe(animationRef.current);
-    }
+		if (animationRef.current) {
+			observer.observe(animationRef.current);
+		}
 
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
-  // let show = true;
-  let processes: string[] = [
-    " Introductory Call",
-    "Tracking Audit",
-    "Data Plan",
-    "Plan Execution",
-    "Wrap-up Meeting",
-  ];
-  return (
-    <ol ref={animationRef} className="infographic">
-      <div className={show ? "enter-up-show rocket" : "enter-up-hidden rocket"}>
-        <svg>
-          <use xlinkHref={"./images/sprite.svg#rocket"}></use>
-        </svg>
-      </div>
-      {processes.map((process, index) => (
-        <li className={show ? "fade-in-show" : "fade-in-hidden"} key={index}>
-          {process}
-        </li>
-      ))}
-    </ol>
-  );
+		return () => {
+			observer.disconnect();
+		};
+	}, []);
+	// let show = true;
+	let processes: string[] = [
+		" Introductory Call",
+		"Tracking Audit",
+		"Data Plan",
+		"Plan Execution",
+		"Wrap-up Meeting",
+	];
+	return (
+		<ol ref={animationRef} className={styles.infographic}>
+			<div
+				className={
+					`${show ? styles.enterUpShow : styles.enterUpHidden}` +
+					" " +
+					styles.rocket
+				}
+			>
+				<svg>
+					<use xlinkHref={"./images/sprite.svg#rocket"}></use>
+				</svg>
+			</div>
+			{processes.map((process, index) => (
+				<li
+					className={`${
+						show ? styles.fadeInShow : styles.fadeInHidden
+					}`}
+					key={index}
+				>
+					{process}
+				</li>
+			))}
+		</ol>
+	);
 };
 
 export default Infographic;
