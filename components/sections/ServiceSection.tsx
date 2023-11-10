41925a10128ffca4e99954e57c90b436028fb44e
vukovicdusan <dusan.v.vukovic@gmail.com>
2023-11-10 22:14:43 +0100

    FaqItem animation fixed and done

--- components/sections/ServiceSection.tsx
+++ components/sections/ServiceSection.tsx
@@ -4,62 +4,68 @@ import ServiceBox from "../ServiceBox";
 import BackgroundLight from "../BackgroundLight";
 
 const ServiceSection = () => {
-  return (
-    <section className="relative mb-10">
-      <BackgroundLight></BackgroundLight>
-      <Wrapper>
-        <h2 className="text-5xl text-primaryAccent text-center mb-10 font-bold">
-          Services
-        </h2>
-        <div className="grid gap-3 grid-cols-[repeat(auto-fit,_minmax(min(400px,_100%),_1fr))]">
-          <ServiceBox
-            service={"Website Tracking"}
-            subservices={[
-              "Event Tracking",
-              "Ecommerce/Product",
-              "Tracking",
-              "Marketing Attribution",
-              "User Tracking",
-              "Funnel Tracking",
-            ]}
-            icon={"website-tracking"}
-          ></ServiceBox>
-          <ServiceBox
-            service={"Tracking Strategy & Documentation"}
-            subservices={["Data Plan", "UTM Tagging Strategy"]}
-            icon={"tracking-strategy"}
-          ></ServiceBox>
-          <ServiceBox
-            service={"Tracking Audit"}
-            subservices={["GA4", "GTM", "Marketing Platforms"]}
-            icon={"tracking-audit"}
-          ></ServiceBox>
-          <ServiceBox
-            service={"Analysis"}
-            subservices={["Marketing", "Website", "Product", "User", "Funnel"]}
-            icon={"analysis"}
-          ></ServiceBox>
-          <ServiceBox
-            service={"Reporting & Visualization"}
-            subservices={["GA4", "Looker Studio"]}
-            icon={"reporting-visualization"}
-          ></ServiceBox>
-          <ServiceBox
-            service={"Server-Side Tracking"}
-            icon={"server-side-tracking"}
-          ></ServiceBox>
-          <ServiceBox
-            service={"Conversion Tracking for Marketing Platforms"}
-            icon={"conversion-tracking"}
-          ></ServiceBox>
-          <ServiceBox
-            service={"Consultation"}
-            icon={"consultation"}
-          ></ServiceBox>
-        </div>
-      </Wrapper>
-    </section>
-  );
+	return (
+		<section className="relative mb-20">
+			<BackgroundLight></BackgroundLight>
+			<Wrapper>
+				<h2 className="text-5xl text-primaryAccent text-center mb-10 font-bold">
+					Services
+				</h2>
+				<div className="grid gap-3 grid-cols-[repeat(auto-fit,_minmax(min(400px,_100%),_1fr))]">
+					<ServiceBox
+						service={"Website Tracking"}
+						subservices={[
+							"Event Tracking",
+							"Ecommerce/Product",
+							"Tracking",
+							"Marketing Attribution",
+							"User Tracking",
+							"Funnel Tracking",
+						]}
+						icon={"website-tracking"}
+					></ServiceBox>
+					<ServiceBox
+						service={"Tracking Strategy & Documentation"}
+						subservices={["Data Plan", "UTM Tagging Strategy"]}
+						icon={"tracking-strategy"}
+					></ServiceBox>
+					<ServiceBox
+						service={"Tracking Audit"}
+						subservices={["GA4", "GTM", "Marketing Platforms"]}
+						icon={"tracking-audit"}
+					></ServiceBox>
+					<ServiceBox
+						service={"Analysis"}
+						subservices={[
+							"Marketing",
+							"Website",
+							"Product",
+							"User",
+							"Funnel",
+						]}
+						icon={"analysis"}
+					></ServiceBox>
+					<ServiceBox
+						service={"Reporting & Visualization"}
+						subservices={["GA4", "Looker Studio"]}
+						icon={"reporting-visualization"}
+					></ServiceBox>
+					<ServiceBox
+						service={"Server-Side Tracking"}
+						icon={"server-side-tracking"}
+					></ServiceBox>
+					<ServiceBox
+						service={"Conversion Tracking for Marketing Platforms"}
+						icon={"conversion-tracking"}
+					></ServiceBox>
+					<ServiceBox
+						service={"Consultation"}
+						icon={"consultation"}
+					></ServiceBox>
+				</div>
+			</Wrapper>
+		</section>
+	);
 };
 
 export default ServiceSection;
--- components/sections/Testimonials.tsx
+++ components/sections/Testimonials.tsx
@@ -3,20 +3,21 @@ import TestimonialSlider from "../TestimonialSlider";
 import Wrapper from "../Wrapper";
 
 const Testimonials = () => {
-  return (
-    <Wrapper>
-      <section className="flex sm:flex-row flex-col">
-        <div className="flex justify-center items-center sm:w-1/4 mb-8 sm:mb-0">
-          <h2 className="text-5xl font-bold">
-            Our <span className="text-primaryAccent">Happy</span> <br></br>Users
-          </h2>
-        </div>
-        <div className="w-full sm:w-3/4 relative">
-          <TestimonialSlider />
-        </div>
-      </section>
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<section className="flex sm:flex-row flex-col mb-20">
+				<div className="flex justify-center items-center sm:w-1/4 mb-8 sm:mb-0">
+					<h2 className="text-5xl font-bold">
+						Our <span className="text-primaryAccent">Happy</span>{" "}
+						<br></br>Users
+					</h2>
+				</div>
+				<div className="w-full sm:w-3/4 relative">
+					<TestimonialSlider />
+				</div>
+			</section>
+		</Wrapper>
+	);
 };
 
 export default Testimonials;
