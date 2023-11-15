b4d6872c3f0a38976671e9765a5fdac4ddd6c7fb
Picvajzmen <picvajzmen@gmail.com>
2023-11-15 09:24:58 +0100

    eslintrc.json change

--- app/api/contact/route.jsx
+++ app/api/contact/route.jsx
@@ -5,37 +5,37 @@ const pass = process.env.NEXT_EMAIL_PASS;
 const mail_to = process.env.NEXT_MAIL_TO;
 
 export const transporter = nodemailer.createTransport({
-	service: "gmail",
-	auth: {
-		user: email,
-		pass: pass,
-	},
+  service: "gmail",
+  auth: {
+    user: email,
+    pass: pass,
+  },
 });
 
 export const mailOptions = {
-	from: email,
-	to: mail_to,
+  from: email,
+  to: mail_to,
 };
 
 // import { mailOptions, transporter } from "../../config/nodemailer";
 
 const handler = async (req, res) => {
-	if (req.method === "POST") {
-		const data = req.body;
+  if (req.method === "POST") {
+    const data = req.body;
 
-		try {
-			await transporter.sendMail({
-				...mailOptions,
-				subject: `Poruka od ${data.email}`,
-				html: `<h2>Poruka od ${data.email}</h2><p>${data.message}</p>`,
-			});
-			return res.status(200).json({ success: true });
-		} catch (err) {
-			console.log(err);
-			return res.status(400).json({ message: err.message });
-		}
-	}
-	res.status(400).json({ message: "Bad Request" });
+    try {
+      await transporter.sendMail({
+        ...mailOptions,
+        subject: `Poruka od ${data.email}`,
+        html: `<h2>Poruka od ${data.email}</h2><p>${data.message}</p>`,
+      });
+      return res.status(200).json({ success: true });
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({ message: err.message });
+    }
+  }
+  res.status(400).json({ message: "Bad Request" });
 };
 
 export default handler;
