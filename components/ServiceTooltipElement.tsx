49fa091cad0885fa95b2fcf1c82682a0e6a2e66e
vukovicdusan <dusan.v.vukovic@gmail.com>
2023-11-29 21:04:00 +0100

    S

--- components/ServiceTooltipElement.tsx
+++ components/ServiceTooltipElement.tsx
@@ -2,61 +2,62 @@
 import React, { useEffect, useRef, useState } from "react";
 
 type TServiceTooltipElement = {
-  title: string;
-  content: string;
+	title: string;
+	content: string;
 };
 
 const ServiceTooltipElement = (props: TServiceTooltipElement) => {
-  const [tooltipShow, setTooltipShow] = useState(false);
-  const [tooltipPosition, setTooltipPosition] = useState("");
-  const ref = useRef<HTMLLIElement>(null);
+	const [tooltipShow, setTooltipShow] = useState(false);
+	const [tooltipPosition, setTooltipPosition] = useState("");
+	const ref = useRef<HTMLLIElement>(null);
 
-  const mouseEnterHandler = () => {
-    setTooltipShow(true);
-    if (ref.current) {
-      let element = ref.current.getBoundingClientRect();
-      let distanceToLeft = element.left;
-      let distanceToRight = window.innerWidth - element.right;
-      distanceToLeft > distanceToRight
-        ? setTooltipPosition("right-0 md:right-full")
-        : setTooltipPosition("-left-12 sm:left-0 md:left-1/2");
-      // console.log(distanceToLeft, distanceToRight);
-    }
-  };
+	const mouseEnterHandler = () => {
+		setTooltipShow(true);
+		if (ref.current) {
+			let element = ref.current.getBoundingClientRect();
+			let distanceToLeft = element.left;
+			let distanceToRight = window.innerWidth - element.right;
+			distanceToLeft > distanceToRight
+				? setTooltipPosition("right-0 md:right-full")
+				: setTooltipPosition("-left-12 sm:left-0 md:left-1/2");
+		}
+	};
 
-  const mouseLeaveHandler = () => {
-    setTooltipShow(false);
-  };
+	const mouseLeaveHandler = () => {
+		setTooltipShow(false);
+	};
 
-  useEffect(() => {
-    ref.current?.addEventListener("mouseenter", mouseEnterHandler);
-    ref.current?.addEventListener("mouseleave", mouseLeaveHandler);
-    return () => {
-      ref.current?.removeEventListener("mouseenter", mouseEnterHandler);
-      ref.current?.removeEventListener("mouseleave", mouseLeaveHandler);
-    };
-  }, []);
-  console.log(tooltipPosition);
-  return (
-    <li
-      ref={ref}
-      className="relative flex flex-nowrap basis-[44%] cursor-pointer underline underline-offset-4 decoration-primaryAccent hover:text-primaryAccent transition-colors"
-    >
-      <svg className="w-[24px] h-[24px] shrink-0">
-        <use xlinkHref={`./images/sprite.svg#dot-line`}></use>
-      </svg>
-      {props.title}
-      <div
-        className={`absolute top-full bg-[rgba(0,0,0,0.3) max-[330px]:w-[25ch]  w-[30ch] sm:w-[40ch] max-h-fit bg-secondaryAccent rounded-3xl border border-primaryAccent p-4
+	useEffect(() => {
+		ref.current?.addEventListener("mouseenter", mouseEnterHandler);
+		ref.current?.addEventListener("mouseleave", mouseLeaveHandler);
+		return () => {
+			ref.current?.removeEventListener("mouseenter", mouseEnterHandler);
+			ref.current?.removeEventListener("mouseleave", mouseLeaveHandler);
+		};
+	}, []);
+
+	return (
+		<li
+			ref={ref}
+			className="relative flex flex-nowrap basis-[44%] cursor-pointer underline underline-offset-4 decoration-primaryAccent hover:text-primaryAccent transition-colors"
+		>
+			<svg className="w-[24px] h-[24px] shrink-0">
+				<use xlinkHref={`./images/sprite.svg#dot-line`}></use>
+			</svg>
+			<button className="contents">{props.title}</button>
+			<div
+				className={`absolute top-full bg-[rgba(0,0,0,0.3) max-[330px]:w-[25ch]  w-[30ch] sm:w-[40ch] max-h-fit bg-secondaryAccent rounded-3xl border border-primaryAccent p-4
                   transition-all duration-200 ease-linear z-10 ${
-                    tooltipShow ? "opacity-100 visible" : "invisible opacity-0"
-                  } ${tooltipPosition}`}
-      >
-        <h4>{props.title}</h4>
-        <p className="text-white">{props.content}</p>
-      </div>
-    </li>
-  );
+						tooltipShow
+							? "opacity-100 visible"
+							: "invisible opacity-0"
+					} ${tooltipPosition}`}
+			>
+				<h4 className="font-bold">{props.title}</h4>
+				<p className="text-white">{props.content}</p>
+			</div>
+		</li>
+	);
 };
 
 export default ServiceTooltipElement;
--- components/sections/ServiceSection.tsx
+++ components/sections/ServiceSection.tsx
@@ -5,150 +5,150 @@ import BackgroundLight from "../BackgroundLight";
 import AnimationContainer from "../AnimationContainer";
 
 const ServiceSection = () => {
-  return (
-    <section id="services" className="relative mb-20">
-      <BackgroundLight></BackgroundLight>
-      <Wrapper>
-        <AnimationContainer direction={"fromBottom"}>
-          <h2 className="text-5xl text-primaryAccent text-center mb-10 font-bold uppercase">
-            Services
-          </h2>
-        </AnimationContainer>
-        <div className="grid gap-3 grid-cols-[repeat(auto-fit,_minmax(min(400px,_100%),_1fr))]">
-          {/* <AnimationContainer direction={"fromLeft"} delay={""}> */}
-          <ServiceBox
-            service={"Website Tracking"}
-            subservices={[
-              [
-                "Event Tracking",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-              [
-                "Ecommerce/Product",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-              [
-                "Tracking",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-              [
-                "Marketing Attribution",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-              [
-                "User Tracking",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-              [
-                "Funnel Tracking",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-            ]}
-            icon={"website-tracking"}
-          ></ServiceBox>
-          {/* </AnimationContainer> */}
-          {/* <AnimationContainer direction={"fromRight"} delay={"delay-100"}> */}
-          <ServiceBox
-            service={"Tracking Strategy & Documentation"}
-            subservices={[
-              [
-                "Data Plan",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-              [
-                "UTM Tagging Strategy",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-            ]}
-            icon={"tracking-strategy"}
-          ></ServiceBox>
-          {/* </AnimationContainer> */}
-          {/* <AnimationContainer direction={"fromLeft"} delay={"delay-200"}> */}
-          <ServiceBox
-            service={"Tracking Audit"}
-            subservices={[
-              [
-                "GA4",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-              [
-                "GTM",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-              [
-                "Marketing Platforms",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-            ]}
-            icon={"tracking-audit"}
-          ></ServiceBox>
-          {/* </AnimationContainer> */}
-          {/* <AnimationContainer direction={"fromRight"} delay={"delay-300"}> */}
-          <ServiceBox
-            service={"Analysis"}
-            subservices={[
-              [
-                "Marketing",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-              [
-                "Website",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-              [
-                "Product",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-              [
-                "User",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-              [
-                "Funnel",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-            ]}
-            icon={"analysis"}
-          ></ServiceBox>
-          {/* </AnimationContainer> */}
-          {/* <AnimationContainer direction={"fromLeft"} delay={"delay-500"}> */}
-          <ServiceBox
-            service={"Reporting & Visualization"}
-            subservices={[
-              [
-                "GA4",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-              [
-                "Looker Studio",
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
-              ],
-            ]}
-            icon={"reporting-visualization"}
-          ></ServiceBox>
-          {/* </AnimationContainer> */}
-          {/* <AnimationContainer direction={"fromRight"} delay={"delay-500"}> */}
-          <ServiceBox
-            service={"Server-Side Tracking"}
-            icon={"server-side-tracking"}
-          ></ServiceBox>
-          {/* </AnimationContainer> */}
-          {/* <AnimationContainer direction={"fromLeft"} delay={"delay-700"}> */}
-          <ServiceBox
-            service={"Conversion Tracking for Marketing Platforms"}
-            icon={"conversion-tracking"}
-          ></ServiceBox>
-          {/* </AnimationContainer> */}
-          {/* <AnimationContainer direction={"fromRight"} delay={"delay-700"}> */}
-          <ServiceBox
-            service={"Consultation"}
-            icon={"consultation"}
-          ></ServiceBox>
-          {/* </AnimationContainer> */}
-        </div>
-      </Wrapper>
-    </section>
-  );
+	return (
+		<section id="services" className="relative mb-20 overflow-x-hidden">
+			<BackgroundLight></BackgroundLight>
+			<Wrapper>
+				<AnimationContainer direction={"fromBottom"}>
+					<h2 className="text-5xl text-primaryAccent text-center mb-10 font-bold uppercase">
+						Services
+					</h2>
+				</AnimationContainer>
+				<div className="grid gap-3 grid-cols-[repeat(auto-fit,_minmax(min(400px,_100%),_1fr))]">
+					{/* <AnimationContainer direction={"fromLeft"} delay={""}> */}
+					<ServiceBox
+						service={"Website Tracking"}
+						subservices={[
+							[
+								"Event Tracking",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+							[
+								"Ecommerce/Product",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+							[
+								"Tracking",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+							[
+								"Marketing Attribution",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+							[
+								"User Tracking",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+							[
+								"Funnel Tracking",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+						]}
+						icon={"website-tracking"}
+					></ServiceBox>
+					{/* </AnimationContainer> */}
+					{/* <AnimationContainer direction={"fromRight"} delay={"delay-100"}> */}
+					<ServiceBox
+						service={"Tracking Strategy & Documentation"}
+						subservices={[
+							[
+								"Data Plan",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+							[
+								"UTM Tagging Strategy",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+						]}
+						icon={"tracking-strategy"}
+					></ServiceBox>
+					{/* </AnimationContainer> */}
+					{/* <AnimationContainer direction={"fromLeft"} delay={"delay-200"}> */}
+					<ServiceBox
+						service={"Tracking Audit"}
+						subservices={[
+							[
+								"GA4",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+							[
+								"GTM",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+							[
+								"Marketing Platforms",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+						]}
+						icon={"tracking-audit"}
+					></ServiceBox>
+					{/* </AnimationContainer> */}
+					{/* <AnimationContainer direction={"fromRight"} delay={"delay-300"}> */}
+					<ServiceBox
+						service={"Analysis"}
+						subservices={[
+							[
+								"Marketing",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+							[
+								"Website",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+							[
+								"Product",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+							[
+								"User",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+							[
+								"Funnel",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+						]}
+						icon={"analysis"}
+					></ServiceBox>
+					{/* </AnimationContainer> */}
+					{/* <AnimationContainer direction={"fromLeft"} delay={"delay-500"}> */}
+					<ServiceBox
+						service={"Reporting & Visualization"}
+						subservices={[
+							[
+								"GA4",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+							[
+								"Looker Studio",
+								"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo obcaecati mollitia repudiandae, quasi quaerat!",
+							],
+						]}
+						icon={"reporting-visualization"}
+					></ServiceBox>
+					{/* </AnimationContainer> */}
+					{/* <AnimationContainer direction={"fromRight"} delay={"delay-500"}> */}
+					<ServiceBox
+						service={"Server-Side Tracking"}
+						icon={"server-side-tracking"}
+					></ServiceBox>
+					{/* </AnimationContainer> */}
+					{/* <AnimationContainer direction={"fromLeft"} delay={"delay-700"}> */}
+					<ServiceBox
+						service={"Conversion Tracking for Marketing Platforms"}
+						icon={"conversion-tracking"}
+					></ServiceBox>
+					{/* </AnimationContainer> */}
+					{/* <AnimationContainer direction={"fromRight"} delay={"delay-700"}> */}
+					<ServiceBox
+						service={"Consultation"}
+						icon={"consultation"}
+					></ServiceBox>
+					{/* </AnimationContainer> */}
+				</div>
+			</Wrapper>
+		</section>
+	);
 };
 
 export default ServiceSection;
