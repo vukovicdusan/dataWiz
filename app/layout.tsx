869141496ba3154d88fdbf59d1d9b8e33b5bb072
Picvajzmen <picvajzmen@gmail.com>
2024-04-05 13:51:57 +0200

    Added all components to the layout.tsx instead of to the pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,18 @@ import { Titillium_Web } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import BackToTop from "@/components/BackToTop";
+import AboutSection from "@/components/sections/AboutSection";
+import CtaSection from "@/components/sections/CtaSection";
+import FaqSection from "@/components/sections/FaqSection";
+import Hero from "@/components/sections/Hero";
+import IndustriesSection from "@/components/sections/IndustriesSection";
+import InfoSection from "@/components/sections/InfoSection";
+import KpiSection from "@/components/sections/KpiSection";
+import MyProcessSection from "@/components/sections/MyProcessSection";
+import ServiceSection from "@/components/sections/ServiceSection";
+import Testimonials from "@/components/sections/Testimonials";
+import Shapedivider from "@/components/Shapedivider";
 
 const titilium = Titillium_Web({
   weight: ["300", "400", "700"],
@@ -25,7 +37,28 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${titilium.className} bg-primaryBg`}>
         <Header />
-        {children}
+        <main className="overflow-hidden">
+          <Hero />
+          <Shapedivider classProp={"translate-y-1"}></Shapedivider>
+          {/* <InfoSection></InfoSection> */}
+          <KpiSection></KpiSection>
+          <Shapedivider
+            classProp={"rotate-180 translate-y-[-5px]"}
+          ></Shapedivider>
+          <ServiceSection></ServiceSection>
+          <IndustriesSection></IndustriesSection>
+          <MyProcessSection></MyProcessSection>
+          <Shapedivider classProp={"translate-y-[5px]"}></Shapedivider>
+          <CtaSection></CtaSection>
+          <Shapedivider
+            classProp={"rotate-180 translate-y-[-5px]"}
+          ></Shapedivider>
+          <Testimonials></Testimonials>
+          <FaqSection></FaqSection>
+          <Shapedivider classProp={"translate-y-[5px]"}></Shapedivider>
+          <AboutSection></AboutSection>
+          <BackToTop></BackToTop>
+        </main>
         <Footer />
       </body>
     </html>
