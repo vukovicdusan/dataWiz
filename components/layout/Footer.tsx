409268df9a89d2076edc0199a581ab0723ae60ef
Picvajzmen <picvajzmen@gmail.com>
2024-04-09 13:35:52 +0200

    Added logo to footer and favicon

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Wrapper from "../Wrapper";
 import Image from "next/image";
-import logo from "../../public/images/full-logo.png";
+import logo from "../../public/images/full-logo-blue.png";
 
 const Footer = () => {
   let date = new Date().getFullYear();
