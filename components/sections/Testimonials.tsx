a1a93adf9eed4543c2bd4dba98ee2454b2d4238b
Picvajzmen <picvajzmen@gmail.com>
2024-04-16 10:49:06 +0200

    Added

--- components/sections/Testimonials.tsx
+++ components/sections/Testimonials.tsx
@@ -2,23 +2,35 @@ import React from "react";
 import TestimonialSlider from "../TestimonialSlider";
 import Wrapper from "../Wrapper";
 import AnimationContainer from "../AnimationContainer";
+import Image from "next/image";
+import logo from "@/public/images/logo-hat-star.png";
 
 const Testimonials = () => {
   return (
     <Wrapper>
-      <section id="testimonials" className="flex sm:flex-row flex-col my-20">
-        <div className="flex justify-center items-center sm:w-1/4 mb-8 sm:mb-0">
+      <section id="testimonials">
+        <AnimationContainer direction={"fromLeft"}>
+          <h2 className="text-5xl font-bold text-center uppercase">
+            Don't Just Take My <span className="text-primaryAccent">Word</span>{" "}
+            <br></br>
+            For It:
+          </h2>
+        </AnimationContainer>
+        <div className="flex sm:flex-row flex-col my-20">
           <AnimationContainer direction={"fromLeft"}>
-            <h2 className="text-5xl font-bold text-center sm:text-left uppercase">
-              Our <span className="text-primaryAccent">Happy</span> <br></br>
-              Users
-            </h2>
+            <Image
+              alt="logo"
+              className="object-contain w-full h-full hidden sm:block"
+              src={logo}
+              placeholder="blur"
+            ></Image>
           </AnimationContainer>
-        </div>
-        <div className="w-full sm:w-3/4 relative">
-          <AnimationContainer direction={"fromRight"}>
-            <TestimonialSlider />
-          </AnimationContainer>
+          <div className="flex justify-center items-center sm:w-1/4 mb-8 sm:mb-0"></div>
+          <div className="w-full sm:w-3/4 relative">
+            <AnimationContainer direction={"fromRight"}>
+              <TestimonialSlider />
+            </AnimationContainer>
+          </div>
         </div>
       </section>
     </Wrapper>
