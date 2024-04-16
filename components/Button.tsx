28072169640845b5456c4eda7084de40cdd2f9e6
Picvajzmen <picvajzmen@gmail.com>
2024-04-16 14:07:14 +0200

    Added capitalize to the button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,7 +7,7 @@ type ButtonPropsType = {
 
 const Button = (props: ChildrenProps & ButtonPropsType) => {
   return (
-    <button className="flex gap-4 rounded-3xl bg-primaryAccent px-3 py-2 group w-fit">
+    <button className="flex gap-4 rounded-3xl bg-primaryAccent px-3 py-2 group w-fit capitalize">
       {props.children}
       <svg className="w-[24px] h-[24px] group-hover:translate-x-1 transition-transform">
         <use xlinkHref={`./images/sprite.svg#button-arrow`}></use>
