f2ee83d75bbb0f1248cc8c2ca0135497e5649e47
Picvajzmen <picvajzmen@gmail.com>
2024-04-17 11:18:28 +0200

    Changed the pathname check statement in herocta

--- components/HeroCta.tsx
+++ components/HeroCta.tsx
@@ -1,16 +1,18 @@
 "use client";
-import React from 'react'
-import ButtonLink from './ButtonLink'
-import { usePathname } from 'next/navigation';
+import React from "react";
+import ButtonLink from "./ButtonLink";
+import { usePathname } from "next/navigation";
 
 const HeroCta = () => {
-    let path = usePathname()
-    let auditCta = "Get your free audit";
-    let consultationCta = "Start Your Success Story";
+  let path = usePathname();
+  let auditCta = "Get your free audit";
+  let consultationCta = "Start Your Success Story";
 
   return (
-    <ButtonLink link={"#cta"}>{path === "/audit" ? auditCta : consultationCta}</ButtonLink>
-  )
-}
+    <ButtonLink link={"#cta"}>
+      {path === "/consultation" ? consultationCta : auditCta}
+    </ButtonLink>
+  );
+};
 
 export default HeroCta;
