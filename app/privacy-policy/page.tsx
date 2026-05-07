b7b3f99c58db77b66106f9228fbd17e9d811fb6f
Dusan <dusan.v.vukovic@gmail.com>
2026-05-07 20:15:05 +0200

    Added content to the privacy-policy page

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,17 +1,174 @@
-import Wrapper from '@/components/Wrapper'
-import React from 'react'
+import Wrapper from "@/components/Wrapper";
+import React from "react";
 
 const PrivacyPolicy = () => {
   return (
-     <Wrapper> 
-        <section className="mt-10 py-10">
-        <div className="mx-auto max-w-4xl text-center">
-            <h1 className="text-3xl font-bold mb-4">Privacy Policy</h1>
-            <p>Page is being built...</p>
-        </div>
-        </section>
-     </Wrapper>
-  )
-}
+    <Wrapper>
+      <section className="mt-10 py-10">
+        <div className="mx-auto max-w-4xl text-center flex flex-col gap-8">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-3xl font-bold ">Privacy Policy</h1>
+            <p>This privacy policy has been compiled to better serve those who are concerned with how their ‘Personally identifiable information’ (PII) is being used online. PII, as used in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">What personal information do we collect from the people that visit our blog, website or app?</h2>
+            <p>When contacting us on our site, you may be asked to enter your name, email address, phone number or other details to help you with your experience.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">When do we collect information?</h2>
+            <p>We collect information from you when you fill out a form or enter information on our site.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">How do we use your information?</h2>
+            <p>We may use the information we collect from you when you register, make a purchase, sign up for our newsletter, respond to a survey or marketing communication, surf the website, or use certain other site features in the following ways:</p>
+            <ul>
+              <li>• To personalize user’s experience and to allow us to deliver the type of content and product offerings in which you are most interested.</li>
+              <li>• To improve our website in order to better serve you.</li>
+              <li>• To allow us to better service you in responding to your customer service requests.</li>
+              <li>• To administer a contest, promotion, survey or other site feature.</li>
+              <li>• To send periodic emails regarding your order or other products and services.</li>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">How do we protect visitor information?</h2>
 
-export default PrivacyPolicy
+            <ul>
+              <li>• We do not use vulnerability scanning and/or scanning to PCI standards.</li>
+              <li>• We do not use Malware Scanning.</li>
+              <li>• We do not use an SSL certificate.</li>
+              <li>• We only provide articles and information on our website. Other than your contact information for the specific purposes of responding to your inquiry, we never ask for personal or private information.</li>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">Do we use cookies?</h2>
+            <p>Yes. Cookies are small files that a site or its service provider transfers to your computer’s hard drive through your Web browser (if you allow) that enables the site’s or service provider’s systems to recognize your browser and capture and remember certain information. For instance, we use cookies to help us understand your preferences based on previous or current site activity, which enables us to provide you with improved services. We also use cookies to help us compile aggregate data about site traffic and site interaction so that we can offer better site experiences and tools in the future.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <p className="font-bold">We use cookies to:</p>
+            <ul>
+              <li>• Understand and save user’s preferences for future visits</li>
+              <li>• Keep track of advertisements.</li>
+              <li>• Compile aggregate data about site traffic and site interactions in order to offer better site experiences and tools in the future. We may also use trusted third party services that track this information on our behalf.</li>
+              <li>• You can choose to have your computer warn you each time a cookie is being sent, or you can choose to turn off all cookies. You do this through your browser (like Internet Explorer) settings. Each browser is a little different, so look at your browser's Help menu to learn the correct way to modify your cookies.</li>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-4">
+            <p className="font-bold">If users disable cookies in their browser:</p>
+            <p>If you disable cookies off, some features will be disabled. It will turn off some of the features that make your site experience more efficient and some of our services will not function properly.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">Third Party Disclosure</h2>
+            <p>
+              We do not sell, trade, or otherwise transfer to outside parties your{" "}
+              personally identifiable information unless we provide you with advance
+              notice.
+            </p>
+            <p>
+              {"This"} does not include website hosting partners and other{" "}
+              {"parties"} who assist us in operating our website,{" "}
+              {"conducting"} our business, or servicing you, so long as those
+              {" parties"} agree to keep this information confidential.
+            </p>
+            <p>We may also release your information when we believe release is appropriate to comply with the law, enforce our site policies, or protect ours or others rights, property, or safety.</p>
+            <p>However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising, or other uses.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">Third party links</h2>
+            <p>Occasionally, at our discretion, we may include or offer third party products or services on our website. These third party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites. Nonetheless, we seek to protect the integrity of our site and welcome any feedback about these sites.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">Google</h2>
+            <p>We use Google Analytics on our website.</p>
+            <p className="font-bold">We have implemented the following:</p>
+            <ul>
+              <li>• Demographics and Interests Reporting</li>
+            </ul>
+            <p>We, along with third-party vendors such as Google, use first-party cookies (such as the Google Analytics cookies) and third-party cookies (such as the DoubleClick cookie) or other third-party identifiers together to compile data regarding user interactions with ad impressions, and other ad service functions as they relate to our website.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">California Online Privacy Protection Act</h2>
+            <p>CalOPPA is the first state law in the nation to require commercial websites and online services to post a privacy policy. The law’s reach stretches well beyond California to require a person or company in the United States (and conceivably the world) that operates websites collecting personally identifiable information from California consumers to post a conspicuous privacy policy on its website stating exactly the information being collected and those individuals with whom it is being shared, and to comply with this policy.</p>
+            <p>See more at: http://consumercal.org/california-online-privacy-protection-act-caloppa/#sthash.0FdRbT51.dpuf</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <p className="font-bold">According to CalOPPA we agree to the following:</p>
+            <ul>
+              <li>• Users can visit our site anonymously.</li>
+              <li>• Once this privacy policy is created, we will add a link to it on our home page, or as a minimum on the first significant page after entering our website.</li>
+              <li>• Our Privacy Policy link includes the word ‘Privacy’, and can be easily be found on the page specified above.</li>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-4">
+            <p className="font-bold">Users will be notified of any privacy policy changes:</p>
+            <ul>
+              <li>• On our Privacy Policy Page</li>
+            </ul>
+            <p className="font-bold">Users are able to change their personal information:</p>
+            <ul>
+              <li>• By emailing us</li>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">How does our site handle do not track signals?</h2>
+            <p>We honor do not track signals and do not track, plant cookies, or use advertising when a Do Not Track (DNT) browser mechanism is in place.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">Does our site allow third party behavioral tracking?</h2>
+            <p>It’s also important to note that we do not allow third party behavioral tracking.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">COPPA (Children Online Privacy Protection Act)</h2>
+            <p>When it comes to the collection of personal information from children under 13, the Children’s Online Privacy Protection Act (COPPA) puts parents in control. The Federal Trade Commission, the nation’s consumer protection agency, enforces the COPPA Rule, which spells out what operators of websites and online services must do to protect children’s privacy and safety online.</p>
+            <p>We do not specifically market to children under 13.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">Fair Information Practices</h2>
+            <p>The Fair Information Practices Principles form the backbone of privacy law in the United States and the concepts they include have played a significant role in the development of data protection laws around the globe. Understanding the Fair Information Practice Principles and how they should be implemented is critical to comply with the various privacy laws that protect personal information.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <p className="font-bold">In order to be in line with Fair Information Practices we will take the following responsive action, should a data breach occur:</p>
+            <p>We will notify the users via email:</p>
+            <ul>
+              <li>• Whithin 7 bussiness days</li>
+            </ul>
+            <p>We also agree to the individual redress principle, which requires that individuals have a right to pursue legally enforceable rights against data collectors and processors who fail to adhere to the law. This principle requires not only that individuals have enforceable rights against data users, but also that individuals have recourse to courts or a government agency to investigate and/or prosecute non-compliance by data processors.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">CAN SPAM Act</h2>
+            <p>The CAN-SPAM Act is a law that sets the rules for commercial email and establishes requirements for commercial messages, gives recipients the right to have emails stopped from being sent to them, and spells out tough penalties for violations.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <p className="font-bold">We collect your email address in order to:</p>
+            <ul>
+              <li>• Send information, respond to inquiries, and/or other requests or questions.</li>
+              <li>• Market to our mailing list or continue to send emails to our clients after the original transaction has occurred.</li>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-4">
+            <p className="font-bold">To be in accordance with CANSPAM we agree to the following:</p>
+            <ul>
+              <li>• NOT use false, or misleading subjects or email addresses.</li>
+              <li>• Identify the message as an advertisement in some reasonable way.</li>
+              <li>• Include the physical address of our business or site headquarters.</li>
+              <li>• Monitor third party email marketing services for compliance, if one is used.</li>
+              <li>• Honor opt-out/unsubscribe requests quickly.</li>
+              <li>• Allow users to unsubscribe by using the link at the bottom of each email.</li>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-4">
+            <p className="font-bold">If at any time you would like to unsubscribe from receiving future emails, you can:</p>
+            <ul>
+              <li>• Follow the instructions at the bottom of each email and we will promptly remove you from ALL correspondence.</li>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold ">CONTACTING US</h2>
+            <p>If there are any questions regarding this privacy policy you may contact us using the information below.</p>
+          </div>
+        </div>
+      </section>
+    </Wrapper>
+  );
+};
+
+export default PrivacyPolicy;
