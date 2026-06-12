7079ffa2f4789d31fe1b45cf2ff781b911592b18
dusan <dusan.v.vukovic@gmail.com>
2026-06-12 13:27:12 +0200

    Header navigation bottom margin fix

--- components/TopNavigation.tsx
+++ components/TopNavigation.tsx
@@ -32,11 +32,11 @@ const TopNavigation = (props: TTopNavigation) => {
     <>
       <nav
         id="mobile-navigation"
-        className={`fixed lg:relative top-0 bottom-0 right-0 mb-16 pl-10 pr-4 py-6 border-l border-l-primaryAccent lg:border-0 h-full w-max flex items-end z-30 backdrop-blur-lg bg-[rgba(46,104,208,.2)] lg:bg-transparent lg:backdrop-blur-0 lg:block transition-all duration-500 ease-in-out ${
+        className={`fixed lg:relative top-0 bottom-0 right-0  pl-10 pr-4 py-6 border-l border-l-primaryAccent lg:border-0 h-full w-max flex items-end z-30 backdrop-blur-lg bg-[rgba(46,104,208,.2)] lg:bg-transparent lg:backdrop-blur-0 lg:block transition-all duration-500 ease-in-out ${
           open ? "translate-x-0" : "translate-x-80 lg:translate-x-0"
         }`}
       >
-        <ul className="flex gap-6 flex-wrap lg:flex-nowrap flex-col lg:flex-row">
+        <ul className="flex gap-6 flex-wrap lg:flex-nowrap flex-col lg:flex-row mb-16 lg:mb-0">
           {props.links.map((link, index) => (
             <li
               key={index}
